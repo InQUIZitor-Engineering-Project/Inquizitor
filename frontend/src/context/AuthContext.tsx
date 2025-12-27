970d16b8631f69a3fdfbc2e9fe8f3f2016834e70
Jakub Karoń <jakub.karon@swmansion.com>
2025-12-27 12:39:13 +0100

    fix deploy

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { loginUser } from "../services/auth";
 import type { Token, UserRead } from "../services/auth";
 import { useLoader } from "../components/Loader/GlobalLoader";
